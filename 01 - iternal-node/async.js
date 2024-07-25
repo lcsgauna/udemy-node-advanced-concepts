1aedc20cecd73f9bfd6cf27375221c0bf1e54872
Lucas Gauna Samudio <lucasgsamudio@gmail.com>
2024-07-24 21:35:49 -0400

    feat: Section 1

---
+++ 01 - iternal-node/async.js
@@ -0,0 +1,20 @@
+const https = require('node:https');
+
+const start = Date.now();
+
+
+function doRequest(){
+    https.request('https://www.google.com', res => {
+        res.on('data', () => {});
+        res.on('end', () => {
+            console.log(Date.now() - start);
+        }); 
+    }).end();
+}
+
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
