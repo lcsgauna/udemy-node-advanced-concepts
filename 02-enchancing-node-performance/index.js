26bb862c168365d56fcd0008d84a2426e48d7067
Lucas Gauna Samudio <lucasgsamudio@gmail.com>
2024-07-29 17:26:40 -0400

    feat: Learning cluster in node

--- 02-enchancing-node-performance/index.js
+++ 02-enchancing-node-performance/index.js
@@ -1,8 +1,30 @@
-const express = require('express');
-const app = express();
+process.env.UV_THREADPOOL_SIZE = 5;
+const cluster = require('node:cluster');
 
-app.get('/',(req,res) => {
-    res.send('Hi there!');
-});
+// Is the file being executed in master mode?
+if(cluster.isMaster){
+    // Cause index.js to be executed *again* but
+    // in child mode
+    cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
+}else{
+    // Child mode, going to act like a server
+    const express = require('express');
+    const crypto = require('node:crypto')
+    const app = express();
+    
+    app.get('/',(req,res) => {
+        crypto.pbkdf2('a', 'b', 100000,512,'sha512', () => {
+            res.send('Hi there!');
+        });
+    });
+    
+    app.get('/fast', (req,res) =>{
+        res.send('This was fast!');
+    })
 
-app.listen(3033);
+    app.listen(3033);
+}
+
